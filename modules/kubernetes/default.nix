bf1a432948354e5fa5b359136ee86424f7d4dbd4
Tristan Schrader <t0rdos@pm.me>
2025-10-26 18:58:04 -0700

    feat: add nixidy cli to devenv shell

--- modules/kubernetes/default.nix
+++ modules/kubernetes/default.nix
@@ -37,11 +37,17 @@ in {
     {perSystem.canivete.opentofu.workspaces.deploy = ./opentofu.nix;}
     (lib.mkIf nixidy.enable {
       perSystem = perSystem @ {
+        inputs',
         pkgs,
+        self',
         system,
         ...
       }: {
-        devenv.shells.default.git-hooks.hooks.lychee.toml.exclude = ["svc.cluster.local"];
+        packages.nixidy = inputs'.nixidy.packages.default;
+        devenv.shells.default = {
+          git-hooks.hooks.lychee.toml.exclude = ["svc.cluster.local"];
+          packages = [self'.packages.nixidy];
+        };
         legacyPackages.nixidyEnvs.${system} = inputs.nixidy.lib.mkEnvs {
           inherit pkgs;
           inherit (nixidy) envs libOverlay;
