656a3f641fd6928454f4d6be5f3e94c93b20efee
Tristan Schrader <tristan@mill.com>
2025-12-14 23:56:47 -0800

    fix: default nixidy.target.rootPath without mkForce

--- modules/kubernetes/nixidy/misc.nix
+++ modules/kubernetes/nixidy/misc.nix
@@ -7,10 +7,13 @@
 }: {
   options.build.scripts.nixidy = can.package "nixidy executable" {internal = true;};
   config.build.scripts.nixidy = perSystem.inputs'.nixidy.packages.default;
-  config.nixidy.target.rootPath = "./generated/nixidy/${config.nixidy.env}";
-  config.nixidy.defaults.helm.transformer = builtins.map (lib.kube.removeLabels [
-    # Helm chart versions are just not necessary
-    "app.kubernetes.io/version"
-    "helm.sh/chart"
-  ]);
+  config.nixidy = {
+    target.branch = lib.mkDefault "trunk";
+    target.rootPath = lib.mkDefault "./generated/nixidy/${config.nixidy.env}";
+    defaults.helm.transformer = builtins.map (lib.kube.removeLabels [
+      # Helm chart versions are just not necessary
+      "app.kubernetes.io/version"
+      "helm.sh/chart"
+    ]);
+  };
 }
