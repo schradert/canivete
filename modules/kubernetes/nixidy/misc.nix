3a361d5ee66f185f5f54adcc335ffc0bcd0da956
Tristan Schrader <tristan@mill.com>
2026-02-16 23:09:20 -0800

    fix(nixidy): target.rootPath override default

--- modules/kubernetes/nixidy/misc.nix
+++ modules/kubernetes/nixidy/misc.nix
@@ -9,7 +9,7 @@
   config.build.scripts.nixidy = perSystem.inputs'.nixidy.packages.default;
   config.nixidy = {
     target.branch = lib.mkDefault "trunk";
-    target.rootPath = lib.mkDefault "./generated/nixidy/${config.nixidy.env}";
+    target.rootPath = lib.mkOverride 500 "./generated/nixidy/${config.nixidy.env}";
     defaults.helm.transformer = builtins.map (lib.kube.removeLabels [
       # Helm chart versions are just not necessary
       "app.kubernetes.io/version"
