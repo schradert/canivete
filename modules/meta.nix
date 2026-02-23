51a254ff4e9d8db053ac93de88380b0cb9270f0d
Tristan Schrader <tristan@mill.com>
2026-02-22 22:25:42 -0800

    feat(meta): default profile email default

--- modules/meta.nix
+++ modules/meta.nix
@@ -1,20 +1,22 @@
 {
   can,
   config,
+  lib,
   ...
 }: {
   options.canivete.meta = {
     domain = can.domain "base domain for exposing nodes and services" {};
     root = can.opt.enum (builtins.attrNames config.canivete.deploy.nodes) "Root server" {};
     people = can.submodule "people in the organization" ({config, ...}: {
-      options.users = can.attrs.submodule "all of the users to create configurations for" {
-        options.name = can.str "name of the user to default to in all contexts" {example = "John Doe";};
+      options.users = can.attrs.submodule "all of the users to create configurations for" ({name, ...}: {
+        options.name = can.str "name of the user to default to in all contexts" {default = name;};
         options.accounts = can.attrs.str "mapping of external program names to user account" {};
         options.profiles = can.attrs.submodule "details on user profiles" {
           options.email = can.email "user profile email" {};
           options.sshPubKey = can.str "public key for connecting to nodes and services and accounts" {};
         };
-      };
+        config.profiles.default.email = lib.mkDefault "${name}@${config.canivete.meta.domain}";
+      });
       options.me = can.enum (builtins.attrNames config.users) "the super admin user in all contexts" {};
       options.my = can.raw "user details associated with 'me'" {default = config.users.${config.me};};
     });
